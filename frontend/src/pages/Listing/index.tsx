4e95797c7cf5d151d4d6005261cd16757ebf1678
luizfilipe29 <luiz_filipe.29@hotmail.com>
2022-03-13 11:45:20 -0300

    UseParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=tittle`)
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
       .then((response) => {
         const data = response.data as MoviePage;
         setPage(data);
